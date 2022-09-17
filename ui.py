552161fd84ad63d6413026fd8285f0720a47b808
TheWalkingSea <austinmccallister0@gmail.com>
2022-09-17 18:55:05 -0400

    Made the ui for the code, including making game logic but havent quite added the actual input output logic
    Co-authored-by: IndianaJonesR <IndianaJonesR@users.noreply.github.com>

---
+++ ui.py
@@ -0,0 +1,70 @@
+import os
+import platform
+from wsgiref.validate import InputWrapper
+from colorama import Fore
+import re
+
+class Red:
+    def __init__(self, word) -> None:
+        self.word = word
+    
+    def __str__(self) -> str:
+        return Fore.RED + self.word
+
+class Green:
+    def __init__(self, word) -> None:
+        self.word = word
+    
+    def __str__(self) -> str:
+        return Fore.GREEN + self.word
+
+class Yellow:
+    def __init__(self, word) -> None:
+        self.word = word
+    
+    def __str__(self) -> str:
+        return Fore.YELLOW + self.word
+def clrscrn():
+    if platform.system() == "Windows":
+        os.system("cls")
+    elif platform.system() == "Darwin" or platform.system() == "Linux":
+        os.system("clear")
+    else:
+        assert False, "OS not supported"
+
+class gamelogic:
+    def __init__(self, CWord) -> None:
+        self.CWord = CWord
+        self.IWords = list()
+    def makeGuess(self, guess):
+        prettyGuess = list() # With colorama.Fore
+        for index, letter in enumerate(guess):
+            if letter == self.CWord[index]: prettyGuess.append(Green(letter))
+            elif letter not in self.CWord: prettyGuess.append(Red(letter))
+            else:
+                duplicateLetters = len(re.findall(letter, self.CWord)) # How many var letter there is in CWord
+                GreenGuess = len(list(filter(lambda x: x == letter, [l for i, l in enumerate(guess) if l == self.CWord[i]]))) # How many Green letters there are in guess
+                YellowGuess = list(filter(lambda x: self.CWord[x] != guess[x], [i for i, l in enumerate(guess) if l in self.CWord and l == letter]))[:(duplicateLetters-GreenGuess)]
+                if index in YellowGuess:
+                    prettyGuess.append(Yellow(letter))
+                else:
+                    prettyGuess.append(Red(letter))
+                # BOOTS CWORD 2 # [3]
+                # IMOoO GUESS 1
+                # 4 [4]
+        self.IWords.append(prettyGuess)
+        return prettyGuess
+    def __str__(self) -> str: [[" 2", "2"], [" 2", "2"], [" 2", "2"]]
+        return "\n".join(["".join(map(str, i)) for i in self.IWords]) + Fore.RESET
+        # return 0
+a = gamelogic("BOOTS")
+a.makeGuess("IMOOO")
+print(a)
+
+
+[str(i) for i in self.yeyeye if i == 2]
+self.yeyeye = []
+i = ["2", "2", "2"]
+for i in self.yeye:
+    if i == 2:
+        yield str(i)
