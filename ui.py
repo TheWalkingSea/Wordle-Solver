dabf3d2b714f1ab66e49aac4d0bd69321ebadae2
TheWalkingSea <austinmccallister0@gmail.com>
2022-09-18 00:45:38 -0400

    Made the wordle main code work and added logic to the input system, also added the win/lose rules
    Co-authored-by: IndianaJonesR <IndianaJonesR@users.noreply.github.com>

--- ui.py
+++ ui.py
@@ -1,6 +1,5 @@
 import os
 import platform
-from wsgiref.validate import InputWrapper
 from colorama import Fore
 import re
 
@@ -24,6 +23,7 @@ class Yellow:
     
     def __str__(self) -> str:
         return Fore.YELLOW + self.word
+        
 def clrscrn():
     if platform.system() == "Windows":
         os.system("cls")
@@ -36,24 +36,24 @@ class gamelogic:
     def __init__(self, CWord) -> None:
         self.CWord = CWord
         self.IWords = list()
-    def makeGuess(self, guess):
-        prettyGuess = list() # With colorama.Fore
+    def makeGuess(self, guess: str):
         for index, letter in enumerate(guess):
-            if letter == self.CWord[index]: prettyGuess.append(Green(letter))
-            elif letter not in self.CWord: prettyGuess.append(Red(letter))
+            if letter == self.CWord[index]: yield Green(letter)
+            elif letter not in self.CWord: yield Red(letter)
             else:
                 duplicateLetters = len(re.findall(letter, self.CWord)) # How many var letter there is in CWord
                 GreenGuess = len(list(filter(lambda x: x == letter, [l for i, l in enumerate(guess) if l == self.CWord[i]]))) # How many Green letters there are in guess
                 YellowGuess = list(filter(lambda x: self.CWord[x] != guess[x], [i for i, l in enumerate(guess) if l in self.CWord and l == letter]))[:(duplicateLetters-GreenGuess)]
                 if index in YellowGuess:
-                    prettyGuess.append(Yellow(letter))
+                    yield Yellow(letter)
                 else:
-                    prettyGuess.append(Red(letter))
+                    yield Red(letter)
                 # BOOTS CWORD 2 # [3]
                 # IMOoO GUESS 1
                 # 4 [4]
-        self.IWords.append(prettyGuess)
-        return prettyGuess
+        # self.IWords.append(prettyGuess)
     def __str__(self) -> str:
         return "\n".join(["".join(map(str, i)) for i in self.IWords]) + Fore.RESET
-        # return 0
+        # return 0h
+
+
